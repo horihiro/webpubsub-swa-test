3e8bad8a3200964824c6fa6c3dbe952ffce37dad
horihiro <hori.kawa.shima.tool+github@gmail.com>
2021-05-19 08:34:07 +0900

    remove timer trigger

--- api/timer/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = async function (context, myTimer) {
-  var timeStamp = new Date().toISOString();
-
-  if (myTimer.IsPastDue)
-  {
-      context.log('Node is running late!');
-  }
-  context.log('Node timer trigger function ran!', timeStamp);   
-};
