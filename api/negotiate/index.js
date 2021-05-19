17d503af9fa5942dd8f55c6929ad161dbfdf4565
horihiro <hori.kawa.shima.tool+github@gmail.com>
2021-05-19 10:36:38 +0900

    change binding name

--- api/negotiate/index.js
+++ api/negotiate/index.js
@@ -1,6 +1,6 @@
 module.exports = async function (context, req) {
   // const signalRConnectionInfo = context.bindings.signalRConnectionInfo;
-  const webPubSubConnectionInfo = context.bindings.webPubSubConnectionInfo;
+  const webPubSubConnectionInfo = context.bindings.connection;
   context.res = {
     body: {
       webPubSubConnectionInfo,
